d0e18d31fefbc125c1016b7871483d5ff4e9f85d
Val | Narmoril <narmoril@tutanota.com>
2022-12-02 19:05:24 +0100

    Initial commit

---
+++ Day 1 - finished/Day 1.py
@@ -0,0 +1,42 @@
+calories = []
+tmp = 0
+
+with open ("input.txt", "r") as file:
+    for line in file:
+        if line.strip() != "":
+            tmp += int(line)
+        else:
+            calories.append(tmp)
+            tmp = 0
+    calories.append(tmp)
+print ("The maximum amount of calories is", max(calories), ".")
+
+top3 = [0,0,0]
+tmp = 0
+
+with open ("input.txt", "r") as file:
+    for line in file:
+        if line.strip() != "":
+            tmp += int(line)
+        else:
+            if top3[0] < tmp:
+                 top3[2] = top3[1]
+                 top3[1] = top3[0]
+                 top3[0] = tmp
+            elif top3[0] < tmp:
+                 top3[2] = top3[1]
+                 top3[1] = tmp
+            elif top3[2] < tmp:
+                 top3[2] = tmp
+            tmp = 0
+    if top3[0] < tmp:
+         top3[2] = top3[1]
+         top3[1] = top3[0]
+         top3[0] = tmp
+    elif top3[0] < tmp:
+         top3[2] = top3[1]
+         top3[1] = tmp
+    elif top3[2] < tmp:
+         top3[2] = tmp
+
+print ("The calorie amount of the top three elves is", (top3[0] + top3[1] + top3[2]), "calories.")
---
+++ Day 2/Day 2.py
@@ -0,0 +1,73 @@
+
+# part 1
+# A, X for rock
+# B, Y for paper
+# C, Z for scissors
+
+total_score = 0
+
+with open ("input.txt", "r") as file:
+    for line in file:
+        if "X" in line.strip():
+            if "A" in line.strip():
+                total_score += 3
+            elif "C" in line.strip():
+                total_score += 6
+            total_score += 1
+
+        elif "Y" in line.strip():
+            if "B" in line.strip():
+                total_score += 3
+            elif "A" in line.strip():
+                total_score += 6
+            total_score += 2
+
+        elif "Z" in line.strip():
+            if "C" in line.strip():
+                total_score += 3
+            elif "B" in line.strip():
+                total_score += 6
+            total_score += 3
+
+print ("The total score would be", total_score, ".")
+
+
+# part 2
+# A for rock
+# B for paper
+# C for scissors
+# X for needing to lose
+# Y for needing to draw
+# Z for needing to win
+
+total_score_correct = 0
+
+with open ("input.txt", "r") as file:
+    for line in file:
+        if "X" in line.strip(): # need to lose
+            if "A" in line.strip(): # scissors
+                total_score_correct += 3
+            elif "B" in line.strip(): # rock
+                total_score_correct += 1
+            elif "C" in line.strip(): # paper
+                total_score_correct += 2
+
+        elif "Y" in line.strip(): # need to draw
+            if "A" in line.strip(): # rock
+                total_score_correct += 1
+            elif "B" in line.strip(): # paper
+                total_score_correct += 2
+            elif "C" in line.strip(): # scissors
+                total_score_correct += 3
+            total_score_correct += 3
+
+        elif "Z" in line.strip(): # need to win
+            if "A" in line.strip(): # paper
+                total_score_correct += 2
+            elif "B" in line.strip(): # scissors
+                total_score_correct += 3
+            elif "C" in line.strip(): # rock
+                total_score_correct += 1
+            total_score_correct += 6
+
+print ("The corrected total score would be", total_score_correct, ".")
